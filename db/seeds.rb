8727188ea9155ab5abb09100433a0642e9ba4977
fabrizzio-gz <fabrizzio@chartmetric.com>
2023-08-21 10:40:20 -0400

    feat: add db:seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+users = User.create([
+  {
+    email: 'admin@blog.com',
+    password: 'admin'
+  },
+  {
+    email: 'user_1@blog.com',
+    password: '123456'
+  }
+])
+
+Post.create([
+  {
+    title: 'An admin blog post',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed faucibus turpis in eu mi bibendum. Ut enim blandit volutpat maecenas. Nulla pharetra diam sit amet nisl suscipit adipiscing bibendum. Pellentesque sit amet porttitor eget dolor.',
+    user_id: users[0].id
+  },
+  {
+    title: 'An author blog post',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed faucibus turpis in eu mi bibendum. Ut enim blandit volutpat maecenas. Nulla pharetra diam sit amet nisl suscipit adipiscing bibendum. Pellentesque sit amet porttitor eget dolor.',
+    user_id: users[1].id
+  }
+])
